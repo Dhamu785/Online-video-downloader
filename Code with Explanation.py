364dd8cc1069ac5455f82efab963e25355ed92f7
Dhamu785 <76835665+Dhamu785@users.noreply.github.com>
2021-06-19 13:21:36 +0530

    Update Code with Explanation.py

--- Code with Explanation.py
+++ Code with Explanation.py
@@ -1,1 +1,25 @@
+#importing the module
+import requests
 
+
+#Size 
+chunk = 256
+
+#Link of the online video
+url = 'Online video link that extension in vdeo formate'
+
+#To get the details of the video
+r= requests.get(url,stream=True)
+
+#To find the total size of the video
+total_size = int(r.headers['content-length'])
+print(total_size)
+
+#To open a file in write bytes mode
+with open('1.mp4', 'wb') as f:
+  
+    #To get the content of the video by bytes
+    for chunk in r.iter_content(chunk_size=chunk):
+      
+        #To write the bytes in the opened file
+        f.write(chunk)
